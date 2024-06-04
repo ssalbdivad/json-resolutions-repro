a744991ad2ed7c5dc612fc577b28fd87d92d2891
David Blass <david@arktype.io>
2024-06-04 17:43:52 -0400

    add more detailed repro info

---
+++ repro.js
@@ -0,0 +1,4 @@
+import foo from "foo/foo.json" with { type: "json" }
+
+console.log(foo.foo)
+
--- repro.ts
+++ repro.ts
@@ -1,3 +1,6 @@
+// this works fine if it's bundled or in old node versions
+// with node 20, you need `assert { type: "json" }`
+// with node 22, you need `with { type: "json" }`
 import foo from "foo/foo.json"
 
 console.log(foo.foo)
